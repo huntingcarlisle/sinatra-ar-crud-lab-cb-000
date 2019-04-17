91a607469223d06ef177434a1e62e1365e927a56
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-04-17 17:42:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,16 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
-  attr_accessor :name, :content
-  POSTS = []
+  # attr_accessor :name, :content
+  # POSTS = []
   
-  def initialize(params)
-    @name = params[:name]
-    @content = params[:content]
-    POSTS << self
-  end
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   POSTS << self
+  # end
   
-  def self.all
-    POSTS
-  end
+  # def self.all
+  #   POSTS
+  # end
 end
